9481b2a1b02215e125fed7e2d6e89dce78f058f3
Dale Page <daleweb@dnlpage.net>
2024-11-18 17:27:08 +0000

    Added some checks and created new add to alignment functions

---
+++ Tools/ADP_Tools.cs
@@ -0,0 +1,19 @@
+﻿using NINA.Astrometry;
+using NINA.Core.Model;
+using NINA.Profile;
+using NINA.WPF.Base.Mediator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADPUK.NINA.AddToAlignmentModel {
+    public class ADP_Tools {
+        public static bool AboveHorizon(Coordinates currentPosition, CustomHorizon horizon, double latitude) {
+            double currentAlt = AstroUtil.GetAltitude(currentPosition.RADegrees, latitude, currentPosition.Dec);
+            double currentAz = AstroUtil.GetAzimuth(currentPosition.RADegrees, currentAlt, latitude, currentPosition.Dec);
+            return (currentAlt > horizon.GetAltitude(currentAz));
+        }
+    }
+}
