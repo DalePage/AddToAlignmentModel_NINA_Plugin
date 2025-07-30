9a6d2d0f58f6167718c33a70ccb579d4bbc2815d
Dale Page <daleweb@dnlpage.net>
2025-07-30 16:12:52 +0100

    Updated for 3.2.0.2001 - New Guid ready for release

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@
 using System.Runtime.InteropServices;
 
 // [MANDATORY] The following GUID is used as a unique identifier of the plugin. Generate a fresh one for your plugin!
-[assembly: Guid("cc7b502f-260f-4fae-b8cc-b252f986e0c8")]
+[assembly: Guid("879618d2-0a61-4b09-b14e-ef2b55583b84")]
 
 // [MANDATORY] The assembly versioning
 //Should be incremented for each new release build of a plugin
@@ -20,28 +20,28 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("ADPUK")]
 // The product name that this plugin is part of
 [assembly: AssemblyProduct("Add To Alignment Model")]
-[assembly: AssemblyCopyright("Copyright © 2024 ADPUK")]
+[assembly: AssemblyCopyright("Copyright © 2025 ADPUK")]
 
 // The minimum Version of N.I.N.A. that this plugin is compatible with
-[assembly: AssemblyMetadata("MinimumApplicationVersion", "3.0.0.2017")]
+[assembly: AssemblyMetadata("MinimumApplicationVersion", "3.2.0.2001")]
 
 // The license your plugin code is using
 [assembly: AssemblyMetadata("License", "MPL-2.0")]
 // The url to the license
 [assembly: AssemblyMetadata("LicenseURL", "https://www.mozilla.org/en-US/MPL/2.0/")]
 // The repository where your pluggin is hosted
-[assembly: AssemblyMetadata("Repository", "https://mypluginsourcerepo.com/project")]
+[assembly: AssemblyMetadata("Repository", "https://github.com/DalePage/AddToAlignmentModel_NINA_Plugin")]
 
 // The following attributes are optional for the official manifest meta data
 
 //[Optional] Your plugin homepage URL - omit if not applicaple
-[assembly: AssemblyMetadata("Homepage", "https://mypluginwebsite.com/")]
+[assembly: AssemblyMetadata("Homepage", "")]
 
 //[Optional] Common tags that quickly describe your plugin
 [assembly: AssemblyMetadata("Tags", "Celestron CPWI Alignment AltAz Alt-Az AZ")]
 
 //[Optional] A link that will show a log of all changes in between your plugin's versions
-[assembly: AssemblyMetadata("ChangelogURL", "https://mypluginsourcerepo.com/project/CHANGELOG.md")]
+[assembly: AssemblyMetadata("ChangelogURL", "")]
 
 //[Optional] The url to a featured logo that will be displayed in the plugin list next to the name
 [assembly: AssemblyMetadata("FeaturedImageURL", "")]
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-﻿# Add To Alignment Model
+﻿# Add To Alignment Model for CPWI Alt-Az Mounts
+This plug-in can be used to create an alignment model for users of CPWI controlled Alt-Az mounts.
 
+The user can select the number of points in both Azimuth and Altitude. Once triggered the mount is moved to the selected
+cordinates and an image obtained and plate solved with the actual RA/Dec then fed back to CPWI as an alignment location.
+
+The plugin has been developed and tested using CPWI with a Celestron Astro-Fi 6 mount and scope. It is beleived it will 
+work with other CPWI controlled Alt Azimuth mounts but it cannot be guranteed. 
